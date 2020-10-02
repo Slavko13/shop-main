f74859f13e9debd7070bb12ef152a4f03fb20921
Slavko <firepoint.pro@gmail.com>
2020-10-02 20:33:41 +0300

    HibernateConfig and DatabasesTools is ready

---
+++ shop-core/base/src/main/java/ru/shop/base/Application.java
@@ -0,0 +1,18 @@
+package ru.shop.base;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    public void start(Class configClass) throws InterruptedException {
+        new AnnotationConfigApplicationContext(configClass);
+        while (true) {
+            Thread.sleep(Long.MAX_VALUE);
+        }
+    }
+
+}
